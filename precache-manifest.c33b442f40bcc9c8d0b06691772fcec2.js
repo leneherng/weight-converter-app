60209a59c58ef84e6f9a774d38f8a2ce5209307d
leneherng <49067812+leneherng@users.noreply.github.com>
2020-06-18 04:22:54 +0800

    Updates

---
+++ precache-manifest.c33b442f40bcc9c8d0b06691772fcec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39caef9dccc16602a64491fea7e8093",
+    "url": "/weight-converter-app/index.html"
+  },
+  {
+    "revision": "2a1b80981e4121dc2258",
+    "url": "/weight-converter-app/static/css/main.c0ed9d3e.chunk.css"
+  },
+  {
+    "revision": "363d67f48389d2c80210",
+    "url": "/weight-converter-app/static/js/2.3cbb2b3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weight-converter-app/static/js/2.3cbb2b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1b80981e4121dc2258",
+    "url": "/weight-converter-app/static/js/main.36b8fbe9.chunk.js"
+  },
+  {
+    "revision": "c4648e51c5c65f340d03",
+    "url": "/weight-converter-app/static/js/runtime-main.2d8a5d6c.js"
+  }
+]);
